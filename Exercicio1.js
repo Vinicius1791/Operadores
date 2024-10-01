80cd929ed599662dfcf3941044314856746a4871
Vinidev1295 <vinidias1295@gmail.com>
2024-09-30 21:11:22 -0300

    dia30

---
+++ Exercicio1.js
@@ -0,0 +1,25 @@
+const primeiroValor = 3
+const segundoValor = 4
+
+const resultado = primeiroValor + segundoValor
+console.log("primeiro cálculo", resultado)
+
+const Valor1 = 3
+const Valor2 = 5
+const Valor3 = 2
+const resultado2 = (Valor1 * Valor2) / Valor3
+console.log("segundo cálculo", resultado2)
+
+const Valor11 = 5
+const Valor22 = 4
+const Valor33 = -1
+const resultado3 = (Valor22-Valor11) *Valor33
+console.log("terceiro cálculo", resultado3)
+
+
+const Valor01 = 234
+const Valor02 = 5
+const resultado4 =Valor01%Valor02
+console.log("quarto cálculo", resultado4)
+
+ 

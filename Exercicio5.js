a0761e31f82d89d1f3e37630250d7fb6a62b8372
Vinicius <vinicius010791@gmail.com>
2024-10-23 20:37:03 -0300

    operadores

---
+++ Exercicio5.js
@@ -0,0 +1,15 @@
+const nome = prompt('Qual o seu nome?')
+const anoDeNasc = Number(prompt('Qual o seu ano de nascimento?'))
+const maiorDeIdade = prompt('Você é maior de idade? Digite true para sim e false para não')
+const anoAtual = 2024
+
+
+
+console.log("Nome:", nome)
+
+console.log("Idade:", anoAtual - anoDeNasc)
+
+console.log("É maior de idade?", maiorDeIdade)
+
+console.log("Idade em 2050:", 2050-1991)
+
